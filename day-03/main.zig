2d49f5261868c0266720e016d3809455a6ca2273
Antonio Lanciotti <a.lanciotti@westernengineering.it>
2025-12-03 10:36:30 +0100

    day 03

---
+++ day-03/main.zig
@@ -0,0 +1,33 @@
+const std = @import("std");
+const input = @embedFile("input.txt");
+
+const delimiters = &std.ascii.whitespace;
+
+fn evalBank(bank: []const u8, digits: usize) usize {
+    std.debug.assert(bank.len >= digits);
+    var val: usize = 0;
+    var idx: usize = 0;
+    for (0..digits) |i| {
+        const curr_bank = bank[idx..(bank.len - (digits - i - 1))];
+        const i_max = std.mem.indexOfMax(u8, curr_bank);
+        val *= 10;
+        val += curr_bank[i_max] - '0';
+        idx += i_max + 1;
+    }
+    return val;
+}
+
+pub fn main() !void {
+    var total_part1: usize = 0;
+    var total_part2: usize = 0;
+
+    var banks = std.mem.tokenizeAny(u8, input, delimiters);
+
+    while (banks.next()) |bank| {
+        total_part1 += evalBank(bank, 2);
+        total_part2 += evalBank(bank, 12);
+    }
+
+    std.debug.print("Part 1 solution: {}\n", .{total_part1});
+    std.debug.print("Part 2 solution: {}\n", .{total_part2});
+}
